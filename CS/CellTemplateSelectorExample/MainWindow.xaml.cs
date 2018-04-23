6befd4d0995354bbdf816b662415a87abab7f9f3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CellTemplateSelectorExample/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.Xpf.Spreadsheet;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CellTemplateSelectorExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            spreadsheetControl1.LoadDocument("vlookup.xlsx");
+            #region #celltemplate_code
+            spreadsheetControl1.CellTemplateSelector = spreadsheetControl1.TryFindResource("CellTemplateSelector") as DataTemplateSelector;
+            #endregion #celltemplate_code
+        }
+    }
+
+    public class CellTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            CellData data = item as CellData;
+            return CanApplyCustomTemplate(data.Cell.Position.ToString()) ? GetCellTemplate(data.Cell, (SpreadsheetControl)container) : base.SelectTemplate(item, container);
+        }
+        List<string> customTemplateCells = new List<string>() { "A2", "A3", "A4", "A5", "A6" };
+        private bool CanApplyCustomTemplate(string cellPosition)
+        {
+            return customTemplateCells.Contains(cellPosition);
+        }
+        private DataTemplate GetCellTemplate(DevExpress.XtraSpreadsheet.Model.Cell cell, SpreadsheetControl control)
+        {
+            string templateName = cell.HasFormula ? "FormulaTemplate" : "EmptyTemplate";
+            return control.TryFindResource(templateName) as DataTemplate;
+        }
+    }
+}
---
+++ VB/CellTemplateSelectorExample/MainWindow.xaml.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.Xpf.Spreadsheet
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace CellTemplateSelectorExample
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			spreadsheetControl1.LoadDocument("vlookup.xlsx")
+'			#Region "#celltemplate_code"
+			spreadsheetControl1.CellTemplateSelector = TryCast(spreadsheetControl1.TryFindResource("CellTemplateSelector"), DataTemplateSelector)
+'			#End Region ' #celltemplate_code
+		End Sub
+	End Class
+
+	Public Class CellTemplateSelector
+		Inherits DataTemplateSelector
+		Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
+			Dim data As CellData = TryCast(item, CellData)
+			Return If(CanApplyCustomTemplate(data.Cell.Position.ToString()), GetCellTemplate(data.Cell, CType(container, SpreadsheetControl)), MyBase.SelectTemplate(item, container))
+		End Function
+		Private customTemplateCells As New List(Of String) (New String() {"A2", "A3", "A4", "A5", "A6"})
+		Private Function CanApplyCustomTemplate(ByVal cellPosition As String) As Boolean
+			Return customTemplateCells.Contains(cellPosition)
+		End Function
+		Private Function GetCellTemplate(ByVal cell As DevExpress.XtraSpreadsheet.Model.Cell, ByVal control As SpreadsheetControl) As DataTemplate
+			Dim templateName As String = If(cell.HasFormula, "FormulaTemplate", "EmptyTemplate")
+			Return TryCast(control.TryFindResource(templateName), DataTemplate)
+		End Function
+	End Class
+End Namespace
